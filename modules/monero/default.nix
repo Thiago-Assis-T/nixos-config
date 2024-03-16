7f91a02aca29407ea9f57d34f934a86cf2b62c1b
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-03-15 22:54:29 -0300

    wezterm tab bar change, and comments for when the blockchain finishes the sync

--- modules/monero/default.nix
+++ modules/monero/default.nix
@@ -2,6 +2,9 @@
 
   imports = [ ];
   environment.systemPackages = with pkgs; [ p2pool xmrig ];
+  # ./p2pool --host 127.0.0.1 --wallet $my_address --mini
+  # wallet: 88d3E2YS7uvWsFtFmSXu8YSQFjLm9qBVvgwC1Bg7xM7cDXAw9xmnTsLhyFy54B95vHCyMaRWtcH1P9NsyzUKDsidUJ3Msn9
+  # ./xmrig -o 127.0.0.1:3333 -u x+50000 --randomx-1gb-pages
 
   services.monero = {
     enable = true;
