8a5afe05e7cf39b4c10c040eed19716d406fc229
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-03-24 23:44:44 -0300

    prep for full migration to dwl!

--- home/default.nix
+++ home/default.nix
@@ -9,15 +9,16 @@
   };
 
   programs.dwl.enable = true;
-
   programs.foot.enable = true;
   programs.foot.package = unstable-pkgs.foot;
+  programs.firefox.enable = true;
 
   home = {
     username = "thiago";
     homeDirectory = "/home/thiago";
     stateVersion = "22.11";
     packages = with pkgs; [ wdisplays bemenu unstable-pkgs.logseq ];
+    sessionVariables = { MOZ_ENABLE_WAYLAND = 1; };
   };
 
 }
