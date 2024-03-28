c124113e4ab83ded04f7aa1b200e51fa25470284
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-03-28 11:28:27 -0300

    linux kernel hardening

---
+++ kernel/hardening.nix
@@ -0,0 +1,105 @@
+{ config, pkgs, ... }: {
+  nix.settings.allowed-users = [ "@users" ];
+
+  environment.memoryAllocator.provider = "scudo";
+  environment.variables.SCUDO_OPTIONS = "ZeroContents=1";
+
+  security = {
+    lockKernelModules = true;
+
+    protectKernelImage = true;
+
+    allowSimultaneousMultithreading = false;
+
+    forcePageTableIsolation = true;
+
+    # This is required by podman to run containers in rootless mode.
+    unprivilegedUsernsClone = config.virtualisation.containers.enable;
+
+    virtualisation.flushL1DataCache = "always";
+
+    apparmor.enable = true;
+    apparmor.killUnconfinedConfinables = true;
+  };
+
+  boot = {
+    kernelParams = [
+      # Don't merge slabs
+      "slab_nomerge"
+
+      # Overwrite free'd pages
+      "page_poison=1"
+
+      # Enable page allocator randomization
+      "page_alloc.shuffle=1"
+
+      # Disable debugfs
+      "debugfs=off"
+    ];
+
+    blacklistedKernelModules = [
+      # Obscure network protocols
+      "ax25"
+      "netrom"
+      "rose"
+
+      # Old or rare or insufficiently audited filesystems
+      "adfs"
+      "affs"
+      "bfs"
+      "befs"
+      "cramfs"
+      "efs"
+      "erofs"
+      "exofs"
+      "freevxfs"
+      "f2fs"
+      "hfs"
+      "hpfs"
+      "jfs"
+      "minix"
+      "nilfs2"
+      "ntfs"
+      "omfs"
+      "qnx4"
+      "qnx6"
+      "sysv"
+      "ufs"
+    ];
+
+    # Hide kptrs even for processes with CAP_SYSLOG
+    kernel.sysctl = {
+      "kernel.kptr_restrict" = 500 2;
+
+      # Disable bpf() JIT (to eliminate spray attacks)
+      "net.core.bpf_jit_enable" = false;
+
+      # Disable ftrace debugging
+      "kernel.ftrace_enabled" = false;
+
+      # Enable strict reverse path filtering (that is, do not attempt to route
+      # packets that "obviously" do not belong to the iface's network; dropped
+      # packets are logged as martians).
+      "net.ipv4.conf.all.log_martians" = true;
+      "net.ipv4.conf.all.rp_filter" = "1";
+      "net.ipv4.conf.default.log_martians" = true;
+      "net.ipv4.conf.default.rp_filter" = "1";
+
+      # Ignore broadcast ICMP (mitigate SMURF)
+      "net.ipv4.icmp_echo_ignore_broadcasts" = true;
+
+      # Ignore incoming ICMP redirects (note: default is needed to ensure that the
+      # setting is applied to interfaces added after the sysctls are set)
+      "net.ipv4.conf.all.accept_redirects" = false;
+      "net.ipv4.conf.all.secure_redirects" = false;
+      "net.ipv4.conf.default.accept_redirects" = false;
+      "net.ipv4.conf.default.secure_redirects" = false;
+      "net.ipv6.conf.all.accept_redirects" = false;
+      "net.ipv6.conf.default.accept_redirects" = false;
+
+      # Ignore outgoing ICMP redirects (this is ipv4 only)
+      "net.ipv4.conf.all.send_redirects" = false;
+      "net.ipv4.conf.default.send_redirects" = false;
+    };
+  };
+}
