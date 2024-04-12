e6e06ffa9fc38228caea61c599e87ed355696f83
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-04-12 19:15:32 -0300

    moving to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS Desktop Config:";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/master";
